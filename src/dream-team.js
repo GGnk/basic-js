da89c796641c3d8f64d7b00b9a2ec042aac277be
GGnk <super-nurlan94@yandex.ru>
2020-03-07 21:30:48 +0400

    fix: done tasks

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,12 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(arr) {
+  if(arr == null || !Array.isArray(arr))  return false;
+
+  let team = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] == "string") {
+      let bigStr = arr[i].trim().toUpperCase();
+      team.push(bigStr[0]);
+    }
+  }
+  return team.sort().join('');
 };
